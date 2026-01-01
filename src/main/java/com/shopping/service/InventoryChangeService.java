47fa79e757d2d6517a105f13b0604b3b4cb1dd3c
robert <test@runoob.com>
2026-01-02 03:17:39 +0800

    调整

--- src/main/java/com/shopping/service/InventoryChangeService.java
+++
@@ -1,8 +0,0 @@
-package com.shopping.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.shopping.entity.InventoryChange;
-
-/** 库存变更服务接口 */
-public interface InventoryChangeService extends IService<InventoryChange> {
-}
